19394d9991416d59232ef692f9c1e68bb05a5ae3
Rachel Solov <rsolov23@yahoo.com>
2021-04-03 09:26:24 -0500

    added README image

---
+++ friendly-parakeet/Develop/README.md
@@ -0,0 +1,3 @@
+![A screenshot of the web page](Screenshot.png) 
+
+
--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -3,36 +3,36 @@ var lowerCase = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o",
 var upperCase = ["A", "B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~","(",")","-","_","=","+","{","}","[","]","<","|",">","?"];
 var numbers = ["1", "2", "3", "4","5","6","7","8","9","0"];
-
+// function to generate password
 function generatePassword() {
 
   var charactersToUse = [];
-  var passwordSize = prompt("pick between 8-128");
+  var passwordSize = prompt("Pick a number between 8-128");
   var password = "";
-
+// while condition to keep unwanted input
   while (passwordSize < 8 || passwordSize > 128) {
-    alert("Please enter a number between 8 and 128");
-    passwordSize = prompt("I said 8 to 128!");
+    alert("Please enter a number between 8 and 128.");
+    passwordSize = prompt("Please enter a number between 8 and 128!");
   }
-
-  var addLower = confirm("do you need lower case characters?");
-  var addUpper = confirm("do you need upper case characters?");
-  var addNumeric = confirm("do you need numeric characters?");
-  var addSpecial = confirm("do you need special characters?");
-
+// variables for confirmation input
+  var addLower = confirm("Do you need lower case characters?");
+  var addUpper = confirm("Do you need upper case characters?");
+  var addNumeric = confirm("Do you need numeric characters?");
+  var addSpecial = confirm("Do you need special characters?");
+// while condition
   while (
     addLower == false &&
     addUpper == false &&
     addNumeric == false &&
-    addSpecial == false
+    addSpecial == false 
   ) {
-    alert("You need to at least pick one condition");
-    addLower = confirm("do you need lower case characters?");
-    addUpper = confirm("do you need upper case characters?");
-    addNumeric = confirm("do you need numeric characters?");
-    addSpecial = confirm("do you need special characters?");
+    alert("Please pick at least one more condition");
+    addLower = confirm("Do you need lower case characters?");
+    addUpper = confirm("Do you need upper case characters?");
+    addNumeric = confirm("Do you need numeric characters?");
+    addSpecial = confirm("Do you need special characters?");
   }
-
+// if statement
   if (addLower) {
     charactersToUse = charactersToUse.concat(lowerCase);
   }
@@ -58,8 +58,7 @@ function generatePassword() {
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -67,3 +66,5 @@ function writePassword() {
 
   passwordText.value = password;
 }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
